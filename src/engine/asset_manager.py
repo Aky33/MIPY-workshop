863774e1a1dbd73d1aa28c3a8afed2671419b03c
Daniel46464 <daniel.kratochvil@tria-tr.cz>
2025-06-14 17:02:19 +0200

    tilemap

--- src/engine/asset_manager.py
+++ src/engine/asset_manager.py
@@ -1,6 +1,7 @@
 import pygame
 
-BASE_IMG_PATH = "assets/"
+BASE_IMG_PATH = "src/assets/"
+TILE_SIZE = 32
 
 class AssetManager:
     asset_path_list = {
@@ -11,10 +12,21 @@ class AssetManager:
     asset_list = {
 
     }
+
+    tileset_path = "tiles/ground_tiles.png"
+
+    tiles = {
+
+    }
         
     def __init__(self):
         for asset in self.asset_path_list:
             self.asset_list[asset] = self.load_image(self.asset_path_list[asset])
+        self.prepare_tiles()
+
+    def prepare_tiles(self):
+        tileset = self.load_image(self.tileset_path)
+        self.tiles["grass"] = tileset.subsurface(0, 0, TILE_SIZE, TILE_SIZE)
 
     def load_image(self, path):
         img = pygame.image.load(BASE_IMG_PATH + path).convert_alpha()
